8ed1531fcba5d89f298974c85730a4d9de6931c1
crishuynh <119613966+CrisH2307@users.noreply.github.com>
2024-07-02 13:43:41 -0400

    update

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://workable-monkfish-26.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
